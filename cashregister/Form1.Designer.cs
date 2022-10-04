80abfca49f606cdd8892bfa418b6c160ecabd5db
Lexi Mesenbrink <leximese197@ed.amdsb.ca>
2022-10-04 10:15:03 -0400

    Add project files.

---
+++ cashregister/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace cashregister
+{
+    partial class guitarlogoPicture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(guitarlogoPicture));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.guitarLabel = new System.Windows.Forms.Label();
+            this.tunerLabel = new System.Windows.Forms.Label();
+            this.stringsLabel = new System.Windows.Forms.Label();
+            this.guitarInput = new System.Windows.Forms.TextBox();
+            this.stringsInput = new System.Windows.Forms.TextBox();
+            this.tunersInput = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.subtotalLabel = new System.Windows.Forms.Label();
+            this.taxamountLabel = new System.Windows.Forms.Label();
+            this.totalpriceLabel = new System.Windows.Forms.Label();
+            this.subtotalOutput = new System.Windows.Forms.Label();
+            this.taxamountOutput = new System.Windows.Forms.Label();
+            this.totalamountOutput = new System.Windows.Forms.Label();
+            this.receiptLabel = new System.Windows.Forms.Label();
+            this.receiptButton = new System.Windows.Forms.Button();
+            this.neworderButton = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.White;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Uighur", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(-3, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(870, 105);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "The Write Note";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // guitarLabel
+            // 
+            this.guitarLabel.AutoSize = true;
+            this.guitarLabel.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guitarLabel.Location = new System.Drawing.Point(12, 135);
+            this.guitarLabel.Name = "guitarLabel";
+            this.guitarLabel.Size = new System.Drawing.Size(170, 34);
+            this.guitarLabel.TabIndex = 1;
+            this.guitarLabel.Text = "Number of Guitars";
+            // 
+            // tunerLabel
+            // 
+            this.tunerLabel.AutoSize = true;
+            this.tunerLabel.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tunerLabel.Location = new System.Drawing.Point(12, 182);
+            this.tunerLabel.Name = "tunerLabel";
+            this.tunerLabel.Size = new System.Drawing.Size(166, 34);
+            this.tunerLabel.TabIndex = 2;
+            this.tunerLabel.Text = "Number of Tuners";
+            // 
+            // stringsLabel
+            // 
+            this.stringsLabel.AutoSize = true;
+            this.stringsLabel.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stringsLabel.Location = new System.Drawing.Point(10, 229);
+            this.stringsLabel.Name = "stringsLabel";
+            this.stringsLabel.Size = new System.Drawing.Size(166, 34);
+            this.stringsLabel.TabIndex = 3;
+            this.stringsLabel.Text = "Number of Strings";
+            // 
+            // guitarInput
+            // 
+            this.guitarInput.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guitarInput.Location = new System.Drawing.Point(198, 137);
+            this.guitarInput.Name = "guitarInput";
+            this.guitarInput.Size = new System.Drawing.Size(73, 30);
+            this.guitarInput.TabIndex = 4;
+            // 
+            // stringsInput
+            // 
+            this.stringsInput.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stringsInput.Location = new System.Drawing.Point(198, 233);
+            this.stringsInput.Name = "stringsInput";
+            this.stringsInput.Size = new System.Drawing.Size(73, 30);
+            this.stringsInput.TabIndex = 5;
+            // 
+            // tunersInput
+            // 
+            this.tunersInput.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tunersInput.Location = new System.Drawing.Point(198, 184);
+            this.tunersInput.Name = "tunersInput";
+            this.tunersInput.Size = new System.Drawing.Size(73, 30);
+            this.tunersInput.TabIndex = 6;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(42, 282);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(203, 35);
+            this.calculateButton.TabIndex = 7;
+            this.calculateButton.Text = "Calculate Totals";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // subtotalLabel
+            // 
+            this.subtotalLabel.AutoSize = true;
+            this.subtotalLabel.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtotalLabel.Location = new System.Drawing.Point(36, 332);
+            this.subtotalLabel.Name = "subtotalLabel";
+            this.subtotalLabel.Size = new System.Drawing.Size(88, 34);
+            this.subtotalLabel.TabIndex = 8;
+            this.subtotalLabel.Text = "Subtotal:";
+            // 
+            // taxamountLabel
+            // 
+            this.taxamountLabel.AutoSize = true;
+            this.taxamountLabel.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxamountLabel.Location = new System.Drawing.Point(12, 375);
+            this.taxamountLabel.Name = "taxamountLabel";
+            this.taxamountLabel.Size = new System.Drawing.Size(119, 34);
+            this.taxamountLabel.TabIndex = 9;
+            this.taxamountLabel.Text = "Tax Amount";
+            // 
+            // totalpriceLabel
+            // 
+            this.totalpriceLabel.AutoSize = true;
+            this.totalpriceLabel.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalpriceLabel.Location = new System.Drawing.Point(12, 422);
+            this.totalpriceLabel.Name = "totalpriceLabel";
+            this.totalpriceLabel.Size = new System.Drawing.Size(131, 34);
+            this.totalpriceLabel.TabIndex = 10;
+            this.totalpriceLabel.Text = "Total Amount";
+            // 
+            // subtotalOutput
+            // 
+            this.subtotalOutput.BackColor = System.Drawing.Color.White;
+            this.subtotalOutput.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtotalOutput.ForeColor = System.Drawing.Color.Black;
+            this.subtotalOutput.Location = new System.Drawing.Point(198, 332);
+            this.subtotalOutput.Name = "subtotalOutput";
+            this.subtotalOutput.Size = new System.Drawing.Size(73, 27);
+            this.subtotalOutput.TabIndex = 11;
+            this.subtotalOutput.Text = "      ";
+            // 
+            // taxamountOutput
+            // 
+            this.taxamountOutput.BackColor = System.Drawing.Color.White;
+            this.taxamountOutput.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxamountOutput.ForeColor = System.Drawing.Color.Black;
+            this.taxamountOutput.Location = new System.Drawing.Point(198, 375);
+            this.taxamountOutput.Name = "taxamountOutput";
+            this.taxamountOutput.Size = new System.Drawing.Size(73, 29);
+            this.taxamountOutput.TabIndex = 12;
+            this.taxamountOutput.Text = "      ";
+            // 
+            // totalamountOutput
+            // 
+            this.totalamountOutput.BackColor = System.Drawing.Color.White;
+            this.totalamountOutput.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalamountOutput.ForeColor = System.Drawing.Color.Black;
+            this.totalamountOutput.Location = new System.Drawing.Point(198, 422);
+            this.totalamountOutput.Name = "totalamountOutput";
+            this.totalamountOutput.Size = new System.Drawing.Size(73, 29);
+            this.totalamountOutput.TabIndex = 13;
+            this.totalamountOutput.Text = "      ";
+            // 
+            // receiptLabel
+            // 
+            this.receiptLabel.BackColor = System.Drawing.Color.White;
+            this.receiptLabel.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiptLabel.Location = new System.Drawing.Point(291, 135);
+            this.receiptLabel.Name = "receiptLabel";
+            this.receiptLabel.Size = new System.Drawing.Size(313, 436);
+            this.receiptLabel.TabIndex = 14;
+            this.receiptLabel.Text = " ";
+            // 
+            // receiptButton
+            // 
+            this.receiptButton.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiptButton.Location = new System.Drawing.Point(42, 496);
+            this.receiptButton.Name = "receiptButton";
+            this.receiptButton.Size = new System.Drawing.Size(203, 35);
+            this.receiptButton.TabIndex = 15;
+            this.receiptButton.Text = "Print Receipt";
+            this.receiptButton.UseVisualStyleBackColor = true;
+            this.receiptButton.Click += new System.EventHandler(this.receiptButton_Click);
+            // 
+            // neworderButton
+            // 
+            this.neworderButton.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.neworderButton.Location = new System.Drawing.Point(631, 496);
+            this.neworderButton.Name = "neworderButton";
+            this.neworderButton.Size = new System.Drawing.Size(203, 35);
+            this.neworderButton.TabIndex = 16;
+            this.neworderButton.Text = "New Order";
+            this.neworderButton.UseVisualStyleBackColor = true;
+            this.neworderButton.Click += new System.EventHandler(this.neworderButton_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::cashregister.Properties.Resources.notes;
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(621, 108);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(223, 108);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::cashregister.Properties.Resources.guitar;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(621, 220);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(223, 270);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // guitarlogoPicture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(866, 589);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.neworderButton);
+            this.Controls.Add(this.receiptButton);
+            this.Controls.Add(this.receiptLabel);
+            this.Controls.Add(this.totalamountOutput);
+            this.Controls.Add(this.taxamountOutput);
+            this.Controls.Add(this.subtotalOutput);
+            this.Controls.Add(this.totalpriceLabel);
+            this.Controls.Add(this.taxamountLabel);
+            this.Controls.Add(this.subtotalLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.tunersInput);
+            this.Controls.Add(this.stringsInput);
+            this.Controls.Add(this.guitarInput);
+            this.Controls.Add(this.stringsLabel);
+            this.Controls.Add(this.tunerLabel);
+            this.Controls.Add(this.guitarLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "guitarlogoPicture";
+            this.Text = "The Write Note";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label guitarLabel;
+        private System.Windows.Forms.Label tunerLabel;
+        private System.Windows.Forms.Label stringsLabel;
+        private System.Windows.Forms.TextBox guitarInput;
+        private System.Windows.Forms.TextBox stringsInput;
+        private System.Windows.Forms.TextBox tunersInput;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label subtotalLabel;
+        private System.Windows.Forms.Label taxamountLabel;
+        private System.Windows.Forms.Label totalpriceLabel;
+        private System.Windows.Forms.Label subtotalOutput;
+        private System.Windows.Forms.Label taxamountOutput;
+        private System.Windows.Forms.Label totalamountOutput;
+        private System.Windows.Forms.Label receiptLabel;
+        private System.Windows.Forms.Button receiptButton;
+        private System.Windows.Forms.Button neworderButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ cashregister/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace cashregister
+{
+    public partial class guitarlogoPicture : Form
+    {
+        //Golbal Varibles 
+        double guitarPrice = 200;
+        int numOfGuitar;
+        double subGuitar;
+
+        double tunerPrice = 15.76;
+        int numOfTuner;
+        double subTuner;
+
+        double stringsPrice = .98;
+        int numOfString;
+        double subString;
+
+        double subTotal;
+        double taxRate = .13;
+        double taxAmount;
+        double totalCost;
+
+        public guitarlogoPicture()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //button noise
+                SoundPlayer elevatorbutton = new SoundPlayer(Properties.Resources.elevatorbutton);
+                elevatorbutton.Play();
+                Refresh();
+                Thread.Sleep(700);
+
+                ////get inputs
+                //slices = Convert.ToInt16(slicesInput.Text);
+                numOfGuitar = Convert.ToInt16(guitarInput.Text);
+                numOfString = Convert.ToInt16(stringsInput.Text);
+                numOfTuner = Convert.ToInt16(tunersInput.Text);
+
+                ////calculations
+                //subtotal = slicePrice * slices;
+                subGuitar = guitarPrice * numOfGuitar;
+                subTuner = tunerPrice * numOfTuner;
+                subString = stringsPrice * numOfString;
+
+                //taxAmount = subtotal * taxRate;
+                //totalAmount = subtotal + taxAmount;
+                subTotal = subGuitar + subTuner + subString;
+                taxAmount = subTotal * taxRate;
+                totalCost = subTotal + taxAmount;
+
+                ////outputs
+                //subtotalOutput.Text = $"{subtotal.ToString("C")}";
+                //taxOutput.Text = $"{taxAmount.ToString("C")}";
+                //totalOutput.Text = $"{totalAmount.ToString("C")}";
+
+                //subtotalOutput.Text = $"{}";
+                subtotalOutput.Text = $"{subTotal.ToString("$.##")}";
+                taxamountOutput.Text = $"{taxAmount.ToString("$.##")}";
+                totalamountOutput.Text = $"{totalCost.ToString("$.##")}";
+            }
+            catch
+            {
+                subtotalOutput.Text = $"Error";
+                taxamountOutput.Text = $"Error";
+                totalamountOutput.Text = $"Error";
+             }
+            //
+
+        }
+
+        private void receiptButton_Click(object sender, EventArgs e)
+        {
+            //create a sound player and load the alert.wav sound, then play it 
+            SoundPlayer elevatorbutton = new SoundPlayer(Properties.Resources.elevatorbutton);
+            elevatorbutton.Play();
+            Refresh();
+            Thread.Sleep(700);
+
+            SoundPlayer printreceipt = new SoundPlayer(Properties.Resources.printreceipt);
+            printreceipt.Play();
+
+            Refresh();
+            Thread.Sleep(500);
+            receiptLabel.Text += $"\n The Write Note";
+            Thread.Sleep(500);
+            receiptLabel.Text += $"\n\n Order Number: 1337";
+            Refresh();
+            Thread.Sleep(500);
+            receiptLabel.Text += $"\n Date: 12-25-06";
+            Refresh();
+            Thread.Sleep(500);
+            receiptLabel.Text += $"\n\n Guitars                                        {numOfGuitar} @ {guitarPrice}";
+            Refresh();
+            Thread.Sleep(500);
+            receiptLabel.Text += $"\n Tuners                                        {numOfTuner} @ {tunerPrice}";
+            Refresh();
+            Thread.Sleep(500);
+            receiptLabel.Text += $"\n Strings                                        {numOfString} @ {stringsPrice}";
+            Refresh();
+            Thread.Sleep(500);
+            receiptLabel.Text += $"\n\n Subtotal                                        {subtotalOutput.Text}";
+            Refresh();
+            Thread.Sleep(500);
+            receiptLabel.Text += $"\n Tax Amount                              {taxamountOutput.Text}";
+            Refresh();
+            Thread.Sleep(500);
+            receiptLabel.Text += $"\n Total                                              {totalamountOutput.Text}";
+            Refresh();
+            Thread.Sleep(500);
+            receiptLabel.Text += $"\n\n Have a good day, don't fret!";
+        }
+
+        private void neworderButton_Click(object sender, EventArgs e)
+        {
+            //create a sound player and load the alert.wav sound, then play it 
+            SoundPlayer elevatorbutton = new SoundPlayer(Properties.Resources.elevatorbutton);
+            elevatorbutton.Play();
+            Refresh();
+            Thread.Sleep(500);
+
+            //reset outputs
+            subtotalOutput.Text = $"";
+            taxamountOutput.Text = $"";
+            totalamountOutput.Text = $"";
+                       
+            //reset receipt
+            receiptLabel.Text = "";
+        }
+               
+    }
+}
